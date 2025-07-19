8114c0b3e52bb026e0ae49312464ab1791e8e1c1
Rafael Alejandro Belalcázar Burbano <rafabeldev@gmail.com>
2025-07-19 15:28:11 +0000

    hide testimonials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
         <Benefits />
         <Pricing />
         <Contact />
-        <Testimonials />
+        {/* <Testimonials /> */}
       </main>
       <Footer />
     </div>
